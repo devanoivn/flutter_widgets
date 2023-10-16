9abb7c3640a75a775e4b86c3c955c6922129b50a
devanoivn <94584742+devanoivn@users.noreply.github.com>
2023-10-16 22:52:57 +0700

    Update favorite_button.dart

--- lib/favorite_button.dart
+++ lib/favorite_button.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+class LoveButton extends StatefulWidget {
+  final VoidCallback onPressed;
+  final bool isLoved;
+
+  LoveButton({required this.onPressed, required this.isLoved});
+
+  @override
+  _LoveButtonState createState() => _LoveButtonState();
+}
+
+class _LoveButtonState extends State<LoveButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        ),
+        child: Center(
+          child: Icon(
+            Icons.favorite,
+            color: widget.isLoved ? Colors.grey : Colors.red,
+            size: 25.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
